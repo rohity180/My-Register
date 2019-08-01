f9315f191b879f133994c034346c8702b02cf6a3
rohit yadav <36644859+rohity180@users.noreply.github.com>
2019-08-01 20:12:20 +0530

    Created Initial Activity for adding expenses and income

---
+++ app/src/main/java/com/mafh/myregister/AddExpense.kt
@@ -0,0 +1,12 @@
+package com.mafh.myregister
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddExpense : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_expense)
+    }
+}
